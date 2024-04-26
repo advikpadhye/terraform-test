0cac0bddcfa654dda3cf85b51083629fc1a53ffc
pravin padhye <pravinpdh867@cgmail.com>
2024-04-26 23:42:25 +0530

    terraform commit

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,3 @@
-provider  "aws" {
-  region  =  "us-east-1"
-  access_key =  "AKIA5IHBEWUG4RS4YHYU"
-  secret_key =  "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"
-  }
-
 resource "aws_instance" "web" {
   ami  = "ami-0e001c9271cf7f3b9"
   instance_type = "t2.micro"
