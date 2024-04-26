1304f6eeeb95a3bb5ca4c5b1127fdeca1e0b38b3
pravin padhye <pravinpdh867@cgmail.com>
2024-04-27 01:42:00 +0530

    update the new changes

--- terraform.tf
+++ terraform.tf
@@ -5,20 +5,20 @@ provider "aws" {
 
  }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "pipeline" {
   ami  = "ami-0e001c9271cf7f3b9"
-  instance_type = "t2.nano"
+  instance_type = "t2.small"
   tags = {
-    Name = "web-server"
+    Name = "web-server-1"
     }
  }
  
 
- resource "aws_instance" "web_socket" {
+ resource "aws_instance" "web_socket_pipeline" {
   ami  = "ami-0e001c9271cf7f3b9"
-  instance_type = "t2.small"
+  instance_type = "t2.micro"
   tags = {
-    Name = "web-socket"
+    Name = "web-socket-1"
     }
  }
 
