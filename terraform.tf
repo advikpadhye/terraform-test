22bdcdd07f48c03bc1b58e40073cebabdc81c99d
pravin padhye <pravinpdh867@cgmail.com>
2024-04-27 01:37:28 +0530

    added new code

--- terraform.tf
+++ terraform.tf
@@ -12,3 +12,13 @@ resource "aws_instance" "web" {
     Name = "web-server"
     }
  }
+ 
+
+ resource "aws_instance" "web_socket" {
+  ami  = "ami-0e001c9271cf7f3b9"
+  instance_type = "t2.small"
+  tags = {
+    Name = "web-socket"
+    }
+ }
+
