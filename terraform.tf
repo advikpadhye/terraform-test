de6fc5c24d6c553afee5426d424a0915b6033da1
pravin padhye <pravinpdh867@cgmail.com>
2024-04-27 01:25:46 +0530

    terraform file modified

--- terraform.tf
+++ terraform.tf
@@ -7,7 +7,7 @@ provider "aws" {
 
 resource "aws_instance" "web" {
   ami  = "ami-0e001c9271cf7f3b9"
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
   tags = {
     Name = "web-server"
     }
